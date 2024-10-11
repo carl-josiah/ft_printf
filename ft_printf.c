14088ab414af9f5c8cdc4bd95dcb9576a861b4b5
Carl Castro <ccastro@lab3r5s9.42abudhabi.ae>
2024-10-11 15:02:38 +0400

    another batch of cleansing

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ccastro <ccastro@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 13:49:31 by ccastro           #+#    #+#             */
-/*   Updated: 2024/10/11 14:10:08 by ccastro          ###   ########.fr       */
+/*   Updated: 2024/10/11 15:02:07 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,23 @@
 
 int	format_specification(va_list ap, char specifier)
 {
-	int	count;
-
-	count = 0;
 	if (specifier == 'c')
-		count += ft_putchar(va_arg(ap, int));
+		return (ft_putchar(va_arg(ap, int)));
 	if (specifier == 's')
-		count += ft_putstr(va_arg(ap, char *));
+		return (ft_putstr(va_arg(ap, char *)));
 	if (specifier == 'p')
-		count += ft_putptr(va_arg(ap, void *));
+		return (ft_putptr(va_arg(ap, void *)));
 	if (specifier == 'd' || specifier == 'i')
-		count += ft_putnbr(va_arg(ap, int));
+		return (ft_putnbr(va_arg(ap, int)));
 	if (specifier == 'u')
-		count += ft_putnbr_uns(va_arg(ap, unsigned long));
+		return (ft_putnbr_uns(va_arg(ap, unsigned long)));
 	if (specifier == 'x')
-		count += ft_puthex_low(va_arg(ap, unsigned int));
+		return (ft_puthex_low(va_arg(ap, unsigned int)));
 	if (specifier == 'X')
-		count += ft_puthex_upp(va_arg(ap, unsigned int));
+		return (ft_puthex_upp(va_arg(ap, unsigned int)));
 	if (specifier == '%')
-		count += write(1, "%%", 1);
-	return (count);
+		return(ft_putchar('%'));
+	return (-1);
 }
 
 int	ft_printf(const char *format, ...)
@@ -46,7 +43,11 @@ int	ft_printf(const char *format, ...)
 	while (*format)
 	{
 		if (*format == '%')
+		{
 			count += format_specification(ap, *(++format));
+			if (count == -1)
+				return (-1);
+		}
 		else
 		{
 			if (write(1, format, 1) == -1)
--- ft_putptr.c
+++ ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: ccastro <ccastro@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 11:49:52 by ccastro           #+#    #+#             */
-/*   Updated: 2024/10/11 11:15:49 by ccastro          ###   ########.fr       */
+/*   Updated: 2024/10/11 14:55:51 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ int	static	ul_to_hex(unsigned long nbr)
 	count = 0;
 	if (nbr >= 16)
 		count += ul_to_hex(nbr / 16);
-	count += ft_putchar("0123456789abcdef"[nbr % 16]);
+	if (write(1, &"0123456789abcdef"[nbr % 16], 1) == -1)
+		return (-1);
+	++count;
 	return (count);
 }
 
@@ -28,7 +30,10 @@ int	ft_putptr(void *ptr)
 	int	count;
 
 	count = 0;
-	count += ft_putstr("0x");
+	if (ft_putstr("0x") == -1)
+		return (-1);
 	count += ul_to_hex((unsigned long) ptr);
-	return (count);
+	if (count == -1)
+		return (-1);
+	return (count + 2);
 }
